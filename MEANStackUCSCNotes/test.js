831226d6c4d2e83f54f017f7d39dbb46ef9c883f
Heggyhere <heggyy@gmail.com>
2018-01-23 14:20:47 -0800

    angular ng

---
+++ MEANStackUCSCNotes/test.js
@@ -0,0 +1,4 @@
+var arrOfNum = [1, 2, 3];
+arrOfNum = ["1", "2"];
+console.log("The number arr is: " + arrOfNum);
+console.log("The number arr is: " + ("" + arrOfNum));
---
+++ MEANStackUCSCNotes/test.ts
@@ -0,0 +1,4 @@
+var arrOfNum: number [] = [1,2,3];
+arrOfNum = ["1", "2"];
+console.log("The number arr is: " + arrOfNum);
+console.log("The number arr is: " + `${arrOfNum}`);
